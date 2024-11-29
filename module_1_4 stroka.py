116ef62309285d110962ac609daab566305e579f
ljusien <ljusien777@gmail.com>
2024-11-29 23:31:25 +0300

    Add files via upload

---
+++ module_1_4 stroka.py
@@ -0,0 +1,10 @@
+name=input('Введите имя:')
+print(name)
+name = 'Denis'
+print(name[0:5])
+print(name.upper())
+print(name.lower())
+print(name.lower())
+name = 'Hello, Denis'
+print(name[0])
+print(name[-1])
